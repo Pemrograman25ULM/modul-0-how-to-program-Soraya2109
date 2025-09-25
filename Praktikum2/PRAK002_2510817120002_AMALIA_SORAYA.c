97ee6d3a89a7108ba86acf95bc463d5552359d4f
soraawistaria <amaliasoraya2109@gmail.com>
2025-09-25 13:37:37 +0800

    Add files via upload

---
+++ Praktikum2/PRAK002_2510817120002_AMALIA_SORAYA.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Selamat Pagi, Amalia Soraya\n");
+    printf("Selamat Siang, Amalia Soraya\n");
+    printf("Selamat Malam, Amalia Soraya\n");
+}
---
+++ Praktikum2/PRAK002_2510817120002_AMALIA_SORAYA.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Amalia Soraya")
+print("Selamat Siang, Amalia Soraya")
+print("Selamat Malam, Amalia Soraya")
