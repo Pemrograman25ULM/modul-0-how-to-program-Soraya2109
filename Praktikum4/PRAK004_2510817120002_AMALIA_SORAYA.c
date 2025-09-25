435aa7aa8ca106f4c00969f01a8eba5374e46d3c
soraawistaria <amaliasoraya2109@gmail.com>
2025-09-25 13:38:38 +0800

    Add files via upload

---
+++ Praktikum4/PRAK004_2510817120002_AMALIA_SORAYA.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("#################\n");
+    printf("#               #\n");
+    printf("# Amalia Soraya #\n");
+    printf("# 2510817120002 #\n");
+    printf("#               #\n");
+    printf("#################\n");
+}
